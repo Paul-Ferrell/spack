57a272355a77430eaab8e8f13431b161d493ef0c
Paul Ferrell <pferrell@lanl.gov>
2021-02-04 13:43:01 -0700

    Just added a unittest for code coverage.

--- lib/spack/spack/test/llnl/util/tty/log.py
+++ lib/spack/spack/test/llnl/util/tty/log.py
@@ -62,6 +62,20 @@ def test_log_python_output_without_echo(capfd, tmpdir):
         assert capfd.readouterr()[0] == ''
 
 
+def test_log_python_output_with_bad_unicode(capfd, tmpdir):
+    with tmpdir.as_cwd():
+        with log_output('foo.txt'):
+            print('logging')
+            print('\xc3\x28Bad unicode\x99')
+            sys.stdout.write(b'\xc3\x28')
+
+        # foo.txt has output
+        with open('foo.txt') as f:
+            assert '<line lost due to invalid unicode>' in f.read()
+
+        # nothing on stdout or stderr
+        assert capfd.readouterr()[0] == ''
+
 def test_log_python_output_and_echo_output(capfd, tmpdir):
     with tmpdir.as_cwd():
         # echo two lines
